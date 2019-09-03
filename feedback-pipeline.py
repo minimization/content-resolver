5479f4e87d3c6cdae607c8c6cf57939cab0d93fd
Adam Samalik <asamalik@redhat.com>
2019-09-03 09:30:58 +0000

    cleanup

--- feedback-pipeline.py
+++ feedback-pipeline.py
@@ -569,14 +569,14 @@ def main():
     args = parser.parse_args()
 
     configs = get_configs(args.input)
-    #installs = install_and_load(configs)
-    installs = load_data("installs-cache.json")
-    #dump_data("installs-cache.json", installs)
+    installs = install_and_load(configs)
+    #installs = load_data("installs-cache.json")
+    dump_data("installs-cache.json", installs)
 
     data = get_data(configs, installs)
 
 
-    #generate_graphs(data, args.output)
+    generate_graphs(data, args.output)
     generate_pages(data, args.output)
     generate_reports_by_base(data, args.output)
     generate_reports_by_use_case(data, args.output)
