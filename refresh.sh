5c1cb785aa754eba6882cc1d31b3c28f50509fca
Adam Samalik <asamalik@redhat.com>
2022-01-31 15:25:52 +0100

    update the refresh script

--- refresh.sh
+++ refresh.sh
@@ -4,7 +4,7 @@
 
 
 
-WORK_DIR=$(mktemp -d -t feedback-pipeline-XXXXXXXXXX)
+WORK_DIR=$(mktemp -d -t content-resolver-XXXXXXXXXX)
 
 if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
   echo "Could not create temp dir"
@@ -21,17 +21,15 @@ trap cleanup EXIT
 cd $WORK_DIR
 
 # Get the latest code repo and configs
-git clone git@github.com:minimization/feedback-pipeline.git || exit 1
-cd feedback-pipeline || exit 1
-git clone git@github.com:minimization/feedback-pipeline-config.git || exit 1
-git clone git@github.com:minimization/content-resolver-input-additional.git || exit 1
-cp -f content-resolver-input-additional/configs/* feedback-pipeline-config/configs/
+git clone https://github.com/minimization/content-resolver || exit 1
+cd content-resolver || exit 1
+git clone https://github.com/minimization/content-resolver-input || exit 1
 
 # Local output dir. Includes a dir for the history data, too.
-mkdir -p $WORK_DIR/feedback-pipeline/out/history || exit 1
+mkdir -p $WORK_DIR/content-resolver/out/history || exit 1
 
 # Get a copy of the historic data
-aws s3 sync s3://tiny.distro.builders/history $WORK_DIR/feedback-pipeline/out/history --exclude "*" --include="historic_data*" || exit 1
+aws s3 sync s3://tiny.distro.builders/history $WORK_DIR/content-resolver/out/history --exclude "*" --include="historic_data*" || exit 1
 
 # Build the site
 build_started=$(date +"%Y-%m-%d-%H%M")
@@ -39,8 +37,8 @@ echo ""
 echo "Building..."
 echo "$build_started"
 echo "(Logging into ~/logs/$build_started.log)"
-CMD="./feedback_pipeline.py --dnf-cache-dir /dnf_cachedir feedback-pipeline-config/configs out" || exit 1
-podman run --rm -it --tmpfs /dnf_cachedir -v $WORK_DIR/feedback-pipeline:/workspace:z asamalik/feedback-pipeline-env $CMD > ~/logs/$build_started.log || exit 1
+CMD="./feedback_pipeline.py --dnf-cache-dir /dnf_cachedir content-resolver-input/configs out" || exit 1
+podman run --rm -it --tmpfs /dnf_cachedir -v $WORK_DIR/content-resolver:/workspace:z localhost/asamalik/fedora-env $CMD > ~/logs/$build_started.log || exit 1
 
 # Publish the site
-aws s3 sync --delete $WORK_DIR/feedback-pipeline/out s3://tiny.distro.builders || exit 1
+aws s3 sync --delete $WORK_DIR/content-resolver/out s3://tiny.distro.builders || exit 1
