f3996c69bf896b3ccf2972d8af6d7ee6f54647cd
Adam Samalik <asamalik@redhat.com>
2024-05-23 17:21:38 +0200

    add a way to run the root log funciton separately

---
+++ test_root_log_function.py
@@ -0,0 +1,15 @@
+from feedback_pipeline import Analyzer
+import urllib
+
+# This is a starting point for a test of the function parsing root logs.
+# Set the url below to any root log, and then you can see what it detected.
+
+root_log_url = "https://kojipkgs.fedoraproject.org//packages/gstreamer1-vaapi/1.22.9/1.fc39/data/logs/x86_64/root.log"
+
+with urllib.request.urlopen(root_log_url) as response:
+    root_log_data = response.read()
+    root_log_contents = root_log_data.decode('utf-8')
+
+required_pkg_names = Analyzer._get_build_deps_from_a_root_log(None, root_log_contents)
+
+print(required_pkg_names)
