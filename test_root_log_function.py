485fb9ae1e97d1ba574fa636d1939caf74259437
Yaakov Selkowitz <yselkowi@redhat.com>
2024-08-19 15:06:25 -0400

    Allow testing against arbitrary root.log
    This allows debugging and further testing against a root.log specified on
    the command line.

--- test_root_log_function.py
+++ test_root_log_function.py
@@ -1,10 +1,16 @@
+#! /usr/bin/python3
+
 from feedback_pipeline import Analyzer
+import sys
 import urllib
 
 # This is a starting point for a test of the function parsing root logs.
 # Set the url below to any root log, and then you can see what it detected.
 
-root_log_url = "https://kojipkgs.fedoraproject.org//packages/gstreamer1-vaapi/1.22.9/1.fc39/data/logs/x86_64/root.log"
+if len(sys.argv) > 1:
+    root_log_url = sys.argv[1]
+else:
+    root_log_url = "https://kojipkgs.fedoraproject.org//packages/gstreamer1-vaapi/1.22.9/1.fc39/data/logs/x86_64/root.log"
 
 with urllib.request.urlopen(root_log_url) as response:
     root_log_data = response.read()
