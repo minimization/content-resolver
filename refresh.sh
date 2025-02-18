27f062f822d4133af73bec5bc30ebb718dfe31b0
Troy Dawson <tdawson@redhat.com>
2025-02-18 14:28:18 -0800

    Update refresh.sh for proper testing

--- refresh.sh
+++ refresh.sh
@@ -2,6 +2,9 @@
 
 # This script runs the content_resolvere.py with the right configs and pushes out the results
 
+### NOTE: Before running, create the dockerfile with
+# podman build -t localhost/asamalik/fedora-env .
+
 
 
 WORK_DIR=$(mktemp -d -t content-resolver-XXXXXXXXXX)
@@ -21,19 +24,15 @@ trap cleanup EXIT
 cd $WORK_DIR
 
 # Get the latest code repo and configs
-git clone https://github.com/minimization/content-resolver || exit 1
+git clone -b buildroot-extras git@github.com:tdawson/content-resolver.git || exit 1
 cd content-resolver || exit 1
-git clone https://github.com/minimization/content-resolver-input || exit 1
+git clone -b no-c10s git@github.com:tdawson/content-resolver-input.git || exit 1
 
 # Local output dir. Includes a dir for the history data, too.
 mkdir -p $WORK_DIR/content-resolver/out/history || exit 1
 
-# Get a copy of the historic data
-aws s3 sync s3://tiny.distro.builders/history $WORK_DIR/content-resolver/out/history --exclude "*" --include="historic_data*" || exit 1
-
-# Get the root log cache
-# (there's no exit one because that file might not exist)
-aws s3 cp s3://tiny.distro.builders/cache_root_log_deps.json $WORK_DIR/content-resolver/cache_root_log_deps.json
+# make sure we have a log dir
+mkdir -p ~/logs/ || exit 1
 
 # Build the site
 build_started=$(date +"%Y-%m-%d-%H%M")
@@ -47,5 +46,3 @@ podman run --rm -it --tmpfs /dnf_cachedir -v $WORK_DIR/content-resolver:/workspa
 # Save the root log cache
 cp $WORK_DIR/content-resolver/cache_root_log_deps.json $WORK_DIR/content-resolver/out/cache_root_log_deps.json || exit 1
 
-# Publish the site
-aws s3 sync --delete $WORK_DIR/content-resolver/out s3://tiny.distro.builders || exit 1
